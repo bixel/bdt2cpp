cddb2e96b7ffc3e16e33aa6ccd811e1bde4c902b
Kevin Heinicke <kevin@kehei.de>
2017-07-18 14:58:38 +0200

    Add the first dummy template

--- bdt2cpp/__init__.py
+++ bdt2cpp/__init__.py
@@ -1,2 +1,11 @@
-def main():
-    return "Hello, world"
+from jinja2 import Template
+from os import path
+
+TEMPLATE_DIR = path.join(path.abspath(path.dirname(__file__)), 'templates')
+
+def main(output_file='main.cpp'):
+    with open(path.join(TEMPLATE_DIR, 'main.cpp'), 'r') as f:
+        template = Template(f.read())
+
+    with open(output_file, 'w') as f:
+        f.write(template.render(foo='bar'))

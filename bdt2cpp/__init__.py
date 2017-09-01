006a0c3177b19b2d1a623cc630087124d2ea632d
Kevin Heinicke <kevin@kehei.de>
2017-09-01 14:55:54 +0200

    Make trees splittable into multiple compilable files
    Also focus on cpp template for now

--- bdt2cpp/__init__.py
+++ bdt2cpp/__init__.py
@@ -1,5 +1,5 @@
 import jinja2
-from os import path, getcwd
+from os import path, getcwd, mkdir
 import re
 import numpy as np
 
@@ -9,9 +9,6 @@ TEMPLATE_DIR = path.join(path.abspath(path.dirname(__file__)), 'templates')
 BRANCH_REGEX = re.compile(f'(?P<branch>\d+):\[(?P<feature>\w+)(?P<comp><)(?P<value>{float_regex})\]')
 LEAF_REGEX = re.compile(f'(?P<leaf>\d+):leaf=(?P<value>{float_regex})')
 FEATURE_REGEX = re.compile('\w(?P<id>\d+)')
-TEMPLATES = {
-    'cpp': 'main.cpp.template',
-}
 
 
 class Node:
@@ -103,14 +100,40 @@ def parse_model(filename):
     return trees
 
 
-def main(input_file, output_file='main.cpp', template='cpp'):
+def split(arr, splits=2):
+    """Split given array into `splits` smaller, similar sized arrays"""
+
+    if len(arr) < splits:
+        raise ValueError("Can't find more splits than array has elements")
+
+    new_size = int(len(arr) / splits)
+    return ([arr[n * new_size:(n+1) * new_size] for n in range(splits-1)]
+            + [arr[(splits-1) * new_size:]])
+
+
+def main(input_file, output_dir='build', trees_per_file=None):
     # template settings
     env = jinja2.Environment(loader=jinja2.PackageLoader('bdt2cpp'),
                              trim_blocks=True, lstrip_blocks=True)
 
-    trees = parse_model(input_file)
+    full_ensemble = parse_model(input_file)
 
-    template = env.get_template(TEMPLATES[template])
+    if trees_per_file and len(full_ensemble) > trees_per_file:
+        trees = split(full_ensemble, int(len(full_ensemble) / trees_per_file))
+    else:
+        trees = [full_ensemble]
 
-    with open(path.join(CUR_DIR, output_file), 'w') as f:
+    if not path.isdir(output_dir):
+        mkdir(output_dir)
+
+    # render subtrees if any
+    if len(trees) > 1:
+        tree_template = env.get_template('standalone.function.template')
+        for i, tree in enumerate(trees):
+            with open(path.join(CUR_DIR, output_dir, f'tree_{i}.cpp'), 'w') as f:
+                f.write(tree_template.render(tree_number=i, tree=tree))
+
+    # render main template
+    template = env.get_template('main.cpp.template')
+    with open(path.join(CUR_DIR, output_dir, 'main.cpp'), 'w') as f:
         f.write(template.render(trees=trees))

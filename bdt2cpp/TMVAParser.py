0750874db4aed08b30546d58001e819bb70bbade
Kevin Heinicke <kevin@kehei.de>
2017-12-13 14:16:26 +0100

    Continue on TMVA parser

--- bdt2cpp/TMVAParser.py
+++ bdt2cpp/TMVAParser.py
@@ -6,8 +6,29 @@ import xml.etree.ElementTree as ET
 class TMVANode(Node):
     def __init__(self, parent=None, xmlNode=None):
         super().__init__(parent=parent)
-        if not parent or xmlNode.nType == 0:
-            self.weight = xmlNode.boostWeight
+        print(xmlNode, xmlNode.get('nType'), xmlNode.items())
+        if not parent or xmlNode.get('nType') == 0:
             self.final = False
-        else xmlNode and xmlNode.nType != 0:
-            self.weight = self.parent.weight * xmlNode.nType
+        elif xmlNode is not None and xmlNode.get('nType') != 0:
+            self.weight = self.parent.weight * xmlNode.get('nType')
+
+        if len(xmlNode) == 2:
+            left, right = list(xmlNode)
+            self.left = TMVANode(self, left)
+            self.right = TMVANode(self, right)
+        elif len(xmlNode) not in [0, 2]:
+            raise(ValueError(f"The node {xmlNode} seems not to be a proper "
+                              "binary tree node"))
+
+
+def parse_model(filename):
+    trees = []
+    with open(filename, 'r') as f:
+        treeXML = ET.fromstring(f.read())
+
+    for tree in treeXML.find('Weights'):
+        firstNode = TMVANode(xmlNode=tree.find('Node'))
+        firstNode.weight = tree.get('boostWeight')
+        trees.append(firstNode)
+
+    return trees

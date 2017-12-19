06d64ed89330b4b09312c026dde5799adaab8278
Kevin Heinicke <kevin@kehei.de>
2017-12-19 16:13:44 +0100

    Add setup.cfg version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(here, 'README.md')) as f:
 
 setup(
     name='bdt2cpp',
-    version='0.0.3',
+    version='0.1.0',
     description='Transpile BDTs to C++ code.',
     long_description=long_description,
     packages=['bdt2cpp'],

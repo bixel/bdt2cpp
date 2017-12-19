547c2c438cf2aa7b745f2b6f3887bf5783c7c38e
Kevin Heinicke <kevin@kehei.de>
2017-12-19 10:06:33 +0100

    Properly parse tmva xml

--- bdt2cpp/TMVAParser.py
+++ bdt2cpp/TMVAParser.py
@@ -4,31 +4,35 @@ import xml.etree.ElementTree as ET
 
 
 class TMVANode(Node):
-    def __init__(self, parent=None, xmlNode=None):
+    def __init__(self, parent=None, xmlNode=None, weight=None):
         super().__init__(parent=parent)
-        print(xmlNode, xmlNode.get('nType'), xmlNode.items())
-        if not parent or xmlNode.get('nType') == 0:
-            self.final = False
-        elif xmlNode is not None and xmlNode.get('nType') != 0:
-            self.weight = self.parent.weight * xmlNode.get('nType')
-
+        self.weight = weight
+        if xmlNode.get('nType'):
+            self.weight *= float(xmlNode.get('nType'))
         if len(xmlNode) == 2:
-            left, right = list(xmlNode)
-            self.left = TMVANode(self, left)
-            self.right = TMVANode(self, right)
+            leftXML, rightXML = list(xmlNode)
+            self.left = TMVANode(self, leftXML, weight)
+            self.right = TMVANode(self, rightXML, weight)
+            self.cut_value = xmlNode.get('Cut')
+            self.feature = xmlNode.get('IVar')
+            self.feature_index = xmlNode.get('IVar')
+            self.final = False
+        elif len(xmlNode) == 0:
+            self.final = True
+
         elif len(xmlNode) not in [0, 2]:
             raise(ValueError(f"The node {xmlNode} seems not to be a proper "
                               "binary tree node"))
 
 
-def parse_model(filename):
+def parse_model(filename, feature_names=None):
     trees = []
     with open(filename, 'r') as f:
         treeXML = ET.fromstring(f.read())
 
     for tree in treeXML.find('Weights'):
-        firstNode = TMVANode(xmlNode=tree.find('Node'))
-        firstNode.weight = tree.get('boostWeight')
+        firstNode = TMVANode(xmlNode=tree.find('Node'),
+                weight=float(tree.get('boostWeight')))
         trees.append(firstNode)
 
     return trees

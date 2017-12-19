c1eb10f6a877b8d8d80722afe5fe974bebdabff8
Kevin Heinicke <kevin@kehei.de>
2017-12-19 10:07:58 +0100

    Add BDT type switch

--- bdt2cpp/__init__.py
+++ bdt2cpp/__init__.py
@@ -8,9 +8,20 @@ import re
 import jinja2
 
 from .XGBoostParser import parse_model as parse_model_xgb
+from .TMVAParser import parse_model as parse_model_tmva
 
 CUR_DIR = getcwd()
 TEMPLATE_DIR = path.join(path.abspath(path.dirname(__file__)), 'templates')
+PARSERS = {
+        'tmva': parse_model_tmva,
+        'xgboost': parse_model_xgb,
+        }
+TYPE_ENDINGS = {
+        '.xgb': 'xgboost',
+        '.xgboost': 'xgboost',
+        '.xml': 'tmva',
+        '.tmva': 'tmva',
+        }
 
 
 def split(arr, splits=2):
@@ -25,7 +36,7 @@ def split(arr, splits=2):
 
 
 def main(input_file, output_dir='build', trees_per_file=None,
-         feature_names=None):
+         feature_names=None, bdt_type=None):
     """
     Read in input file, render templates and write compilable files to
     output_dir.
@@ -34,7 +45,13 @@ def main(input_file, output_dir='build', trees_per_file=None,
     env = jinja2.Environment(loader=jinja2.PackageLoader('bdt2cpp'),
                              trim_blocks=True, lstrip_blocks=True)
 
-    full_ensemble = parse_model_xgb(input_file, feature_names)
+    # extract the bdt type if none is specified explicitly
+    if not bdt_type:
+        _, ending = path.splitext(input_file)
+        bdt_type = TYPE_ENDINGS[ending]
+
+    # parse the bdt file
+    full_ensemble = PARSERS[bdt_type](input_file, feature_names)
 
     if trees_per_file and len(full_ensemble) > trees_per_file:
         trees = split(full_ensemble, int(len(full_ensemble) / trees_per_file))

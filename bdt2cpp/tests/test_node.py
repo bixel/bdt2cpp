2a4f0faa4d18a9a2ce9dd9d7e954c63dbd614e37
Kevin Heinicke <kevin.heinicke@tu-dortmund.de>
2020-03-02 16:33:58 +0100

    Update test_node for pytest

--- bdt2cpp/tests/test_node.py
+++ bdt2cpp/tests/test_node.py
@@ -1,36 +1,33 @@
-import unittest
 from sklearn.datasets import make_classification
 from xgboost import XGBClassifier
+import numpy as np
 
-import bdt2cpp
+from bdt2cpp.XGBoostParser import XGBoostNode
 
 
-class TestNode(unittest.TestCase):
-    def setUp(self):
+class TestNode:
+    @classmethod
+    def setup_class(self):
         X, y = make_classification(n_features=5, random_state=1)
         self.classifier = XGBClassifier(n_estimators=3)
         self.classifier.fit(X, y)
         self.predictions = self.classifier.predict_proba(X)
-        self.model_dump = [tree.split('\n') for tree in self.classifier.booster().get_dump()]
+        print(self.classifier.get_booster().get_dump())
+        self.model_dump = [tree.split('\n') for tree in self.classifier.get_booster().get_dump()]
 
     def test_parse_root_node(self):
-        node = bdt2cpp.Node(self.model_dump[0][0])
-        self.assertEqual(node.cut_value, -0.464102)
-        self.assertFalse(node.weight)
-        self.assertEqual(node.root, node)
-        self.assertIsNone(node.parent)
+        node = XGBoostNode(line=self.model_dump[0][0])
+        assert np.equal(node.cut_value, -0.464102179)
+        assert not node.weight
+        assert node.root == node
+        assert node.parent is None
         return node
 
     def test_parse_left_node(self):
         node = self.test_parse_root_node()
         root = node
-        node.left = bdt2cpp.Node(self.model_dump[0][1], parent=node)
+        node.left = XGBoostNode(line=self.model_dump[0][1], parent=node)
         node = node.left
-        self.assertEqual(node.parent, root)
-        self.assertFalse(node.cut_value)
-        self.assertEqual(node.weight, -0.184906)
-        return node
-
-
-if __name__ == '__main__':
-    unittest.main()
+        assert node.parent == root
+        assert not node.cut_value
+        assert np.equal(node.weight, -0.554717004)
